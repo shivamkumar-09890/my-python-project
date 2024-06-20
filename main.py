773c90795d4ef7d6daaf7a0448400f34ea7f8f52
shivamkumar <shivamkumar878758@gmail.com>
2024-06-21 02:30:04 +0530

    again a change

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
 import requests
 import csv
+import os
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # For flash messages
+
+# URLs to your hosted ToS and Privacy Policy
+TOS_URL = "https://shivamkumar-09890.github.io/app-doc/tos.html"
+PRIVACY_POLICY_URL = "https://shivamkumar-09890.github.io/app-doc/privacy-policy.html"
 
 def fetch_facebook_page_data(page_id, access_token):
     url = f"https://graph.facebook.com/{page_id}?access_token={access_token}"
@@ -28,14 +37,19 @@ def save_to_csv(data, filename='facebook_page_data.csv'):
         # Writing the data
         csvwriter.writerows(rows)
 
-#if __name__ == "__main__":
-    #page_id = "your_facebook_page_id"
-    #access_token = "your_facebook_access_token"
-    #data = fetch_facebook_page_data(page_id, access_token)
-    #if data:
-        #save_to_csv(data)
-        #print("Data saved to facebook_page_data.csv")
-    #else:
-        #print("Failed to fetch data")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        page_id = request.form['page_id']
+        access_token = request.form['access_token']
+        data = fetch_facebook_page_data(page_id, access_token)
+        if data:
+            save_to_csv(data)
+            flash('Data saved to facebook_page_data.csv', 'success')
+        else:
+            flash('Failed to fetch data', 'danger')
+        return redirect(url_for('index'))
+    return render_template('index.html', tos_url=TOS_URL, privacy_policy_url=PRIVACY_POLICY_URL)
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
